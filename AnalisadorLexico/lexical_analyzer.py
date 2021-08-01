be4db0ab3acbe489434eb798af349956759e200e
Bruno <brunoborges2001@gmail.com>
2021-08-01 18:35:51 -0300

    acho que esta pronto

--- AnalisadorLexico/lexical_analyzer.py
+++ AnalisadorLexico/lexical_analyzer.py
@@ -3,6 +3,78 @@ from AF import *
 from ER import *
 
 
+def insert_token_file(filename):
+    token_file = open(filename)
+
+    token_er_dict = {}
+
+    token_er_dict["reserved_words"] = None
+
+    reserved_words = {}
+
+    ignore_er = []
+
+    for line in token_file.readlines():
+        line = line.replace("\n", "")
+        if len(line) == 0:
+            continue
+
+        if line[0] == "#":
+            continue
+
+        if line[0] == "\"":
+            ignore_er.append(ER("-", line[1:-1]).get_afd())
+            continue
+
+        [fp, sp] = line.split("=", 1)
+
+        if ":" in sp and sp[sp.index(":") - 1] != "\\":
+            [pattern, er_string] = sp.split(":", 1)
+
+            er_string = er_string[1:-1]
+
+            if pattern in token_er_dict.keys() and token_er_dict[pattern].test_input(er_string)[-1]:
+                reserved_words[fp] = er_string
+
+        else:
+            token_er_dict[fp] = ER(fp, sp[1:-1]).get_afd()
+
+    token_file.close()
+
+    token_er_dict["reserved_words"] = ER(
+        "RW", '|'.join(reserved_words.values())).get_afd()
+
+    ERs = list(token_er_dict.values())
+
+    if len(ERs) == 0:
+        exit()
+    elif len(ERs) == 1:
+        afnd = ERs[0]
+    elif len(ERs) >= 2:
+        afnd = union(ERs[0], ERs[1])
+        for er in ERs[2:]:
+            afnd.print_transition_table("debug/Test.txt")
+            afnd = union(afnd, er)
+
+    er_afd = afnd.determinize()
+
+    ig_afnd = None
+
+    if len(ignore_er) == 1:
+        ig_afnd = ignore_er[0]
+    elif len(ignore_er) >= 2:
+        ig_afnd = union(ignore_er[0], ignore_er[1])
+        for i in ignore_er[2:]:
+            ig_afnd = union(ig_afnd, i)
+
+    ignored_afd = ig_afnd.determinize()
+
+    afd = union(er_afd, ignored_afd).determinize("S")
+    afd.print_transition_table("debug/AFD.txt")
+
+    return afd
+
+
 def start_lexical_analyzer(file_path, afd):
 
     file = open(file_path, 'r')
@@ -35,8 +107,14 @@ def start_lexical_analyzer(file_path, afd):
                 add_symbol = False
                 lexeme_begin = i
             elif state == "error state":
-                raise Exception(f"Erro na posição {i}")
+                print(f"Erro na posição {i}")
+                break
             else:
                 i += 1
 
-    print(symbol_table)
+    row_format = "{:>20}" * 2
+
+    print(row_format.format("Lexema", "Token"))
+
+    for i in symbol_table:
+        print(row_format.format(*i))
--- main.py
+++ main.py
@@ -3,81 +3,12 @@ from AF import *
 from ER import *
 from AnalisadorLexico import *
 
-token_file = open("debug/tokens.txt")
+print("Insert tokens file name")
+t_filename = input("> ")
+# t_filename = "debug/tokens.txt"
+afd = insert_token_file(t_filename)
 
-token_er_dict = {}
-token_er_dict["reserved_words"] = None
-
-reserved_words = {}
-
-ignore_er = []
-
-for line in token_file.readlines():
-    if "\n" in line:
-        line = line[:-1]
-    if len(line) == 0:
-        continue
-
-    if line[0] == "#":
-        continue
-
-    if line[0] == "\"":
-        ignore_er.append(ER("-", line[1:-1]).get_afd())
-        continue
-
-    [fp, sp] = line.split("=", 1)
-
-    if ":" in sp and sp[sp.index(":") - 1] != "\\":
-        [pattern, er_string] = sp.split(":", 1)
-
-        er_string = er_string[1:-1]
-
-        if pattern in token_er_dict.keys() and token_er_dict[pattern].test_input(er_string)[-1]:
-            reserved_words[fp] = er_string
-
-    else:
-        token_er_dict[fp] = ER(fp, sp[1:-1]).get_afd()
-
-token_file.close()
-
-token_er_dict["reserved_words"] = ER(
-    "RW", '|'.join(reserved_words.values())).get_afd()
-
-ERs = list(token_er_dict.values())
-
-if len(ERs) == 0:
-    exit()
-elif len(ERs) == 1:
-    afnd = ERs[0]
-elif len(ERs) >= 2:
-    afnd = union(ERs[0], ERs[1])
-    for er in ERs[2:]:
-        afnd.print_transition_table("debug/Test.txt")
-        afnd = union(afnd, er)
-
-er_afd = afnd.determinize()
-
-ig_afnd = None
-
-if len(ignore_er) == 1:
-    ig_afnd = ignore_er[0]
-elif len(ignore_er) >= 2:
-    ig_afnd = union(ignore_er[0], ignore_er[1])
-    for i in ignore_er[2:]:
-        ig_afnd = union(ig_afnd, i)
-
-
-ignored_afd = ig_afnd.determinize()
-
-afd = union(er_afd, ignored_afd).determinize("S")
-afd.print_transition_table("debug/AFD.txt")
-
-# TODO: Por algum motivo o operador ":" nao eh reconhecido pelo AFD.
-# TODO: Eh possivel juntar o er_afd e ignored_afd em um unico AFD?
-# Se for possivel, ficaria mais facil pois nao daria problema de NoneType
-# TODO: Por algum motivo o operador "def" eh reconhecido como id
-
-# print(er_afd.test_input(':'))
-# print(er_afd.text_input('def'))
-# print(er_afd.test_input('teste:'))
-start_lexical_analyzer("debug/input.txt", afd)
+print("Insert source file name")
+s_filename = input("> ")
+# s_filename = "debug/input.txt"
+start_lexical_analyzer(s_filename, afd)
