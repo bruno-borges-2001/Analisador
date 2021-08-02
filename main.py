f5ad4a358a8417c73c609b1cf3c24fe839c2155e
ErikSugawara <erik.sugawara@grad.ufsc.br>
2021-08-01 22:52:09 -0300

    Adicionado interface grafica

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from Operations import *
 from AF import *
 from ER import *
 from AnalisadorLexico import *
+from GUI import *
 
-print("Insert tokens file name")
-t_filename = input("> ")
+myGUI = GUI()
+
+#print("Insert tokens file name")
+#t_filename = input("> ")
 # t_filename = "debug/tokens.txt"
-afd = insert_token_file(t_filename)
+#afd = insert_token_file(t_filename)
 
-print("Insert source file name")
-s_filename = input("> ")
+#print("Insert source file name")
+#s_filename = input("> ")
 # s_filename = "debug/input.txt"
-start_lexical_analyzer(s_filename, afd)
+#start_lexical_analyzer(s_filename, afd)
